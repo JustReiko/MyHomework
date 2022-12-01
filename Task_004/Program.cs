f2a96cdf44fed2f98667a3192dcda187100b90d4
JustReiko <vellarym@gmail.com>
2022-12-01 08:17:32 +0500

    task_004 added

---
+++ Task_004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+string first = Console.ReadLine() ?? "";
+int n = Convert.ToInt32(first);
+
+for (int i = 1; i <= n; i++)
+if (i % 2 == 0)
+{
+    Console.WriteLine(i);
+}
