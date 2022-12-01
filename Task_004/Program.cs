9e0fa6fb0df3b3d1dc5e30e72fcb9f4b70a3e502
JustReiko <vellarym@gmail.com>
2022-12-01 08:34:50 +0500

    formatting

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -3,7 +3,7 @@ string first = Console.ReadLine() ?? "";
 int n = Convert.ToInt32(first);
 
 for (int i = 1; i <= n; i++)
-if (i % 2 == 0)
-{
-    Console.WriteLine(i);
-}
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
