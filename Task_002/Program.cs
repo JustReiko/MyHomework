42693b0e09e01a2924a7747ffd1bc5ef82cb3401
JustReiko <vellarym@gmail.com>
2022-12-01 07:15:03 +0500

    task002 added added

---
+++ Task_002/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число:");
+string first = Console.ReadLine() ?? "";
+int n = Convert.ToInt32(first);
+Console.WriteLine("Введите второе число:");
+string second = Console.ReadLine() ?? "";
+int m = Convert.ToInt32(second);
+Console.WriteLine("Введите третье число:");
+string third = Console.ReadLine() ?? "";
+int o = Convert.ToInt32(third);
+int max = 0;
+if (n > max) {
+    max = n;
+}
+
+if (o > max) {
+
+    max = o;
+}
+
+if (m > max) {
+    max = m;
+
+}
+Console.WriteLine($"Большее число {max}");
