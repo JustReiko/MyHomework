55602a4f50c7aa02e2ab6217b63a094f2d0a6069
JustReiko <vellarym@gmail.com>
2022-12-01 07:40:41 +0500

    task003 added

---
+++ Task_003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число:");
+string first = Console.ReadLine() ?? "";
+int n = Convert.ToInt32(first);
+if ((n % 2) == 0) {
+    Console.WriteLine("Это четное число");
+}
+else {
+    Console.WriteLine("Это нечетное число");
+}
