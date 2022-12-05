097d81b4ac189f3d9e5e6be6c6b1c0cabf81a3f6
JustReiko <vellarym@gmail.com>
2022-12-05 09:45:13 +0500

    First task solution added

---
+++ Seminar_3/Task_001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите число:");
+string first = Console.ReadLine() ?? "";
+int n = Convert.ToInt32(first);
+string stringNumber = Convert.ToString(n);
+Console.WriteLine("Вторая цифра данного числа -> "+stringNumber[1]);
