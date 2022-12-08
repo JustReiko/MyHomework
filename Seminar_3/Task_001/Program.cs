5515385dbbdd063844a0e3061eba83fa8ff23cf7
JustReiko <vellarym@gmail.com>
2022-12-08 14:30:11 +0500

    seminar 3 1st task added

---
+++ Seminar_3/Task_001/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+if (length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - это число Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - это число НЕ Палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($" {number}  - это не пятизначное число");
+}
+
+
